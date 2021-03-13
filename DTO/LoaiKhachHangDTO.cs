0abc0f14b2a959a4e37daf87cbd544a93ade50ab
QuangThienSix <41982539+QuangThienSix@users.noreply.github.com>
2021-03-13 13:11:02 +0700

    thêm danh mục phòng và phieu thuê DTO

---
+++ DTO/LoaiKhachHangDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class LoaiKhachHangDTO
+    {
+        int _maLoaiKhachHang;
+        string _loaiKhach;
+        float _heSoKhach;
+
+        public int MaLoaiKhachHang { get => _maLoaiKhachHang; set => _maLoaiKhachHang = value; }
+        
+        public float HeSoKhach { get => _heSoKhach; set => _heSoKhach = value; }
+        public string LoaiKhach { get => _loaiKhach; set => _loaiKhach = value; }
+    }
+}
