2934326520120b36085c385f75afcc814d512bba
tranquoctrung1 <tranquoctrung.12k9@gmail.com>
2021-03-13 14:22:21 +0700

    add baocaomatdo screen

---
+++ GUI/TruyenDuLieuManHinh.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Management.GUI
+{
+    public class TruyenDuLieuManHinh
+    {
+        public static string MaPhong = null;
+        public static string Thang = null;
+        public static PhongDTO p = new PhongDTO();
+        public static LoaiPhongDTO lp = new LoaiPhongDTO();
+        public static LoaiTinhTrangDTO tt = new LoaiTinhTrangDTO();
+        public static KhachHangDTO kh = new KhachHangDTO();
+        public static LoaiKhachHangDTO lkh = new LoaiKhachHangDTO();
+        public static ChiTietHoaDonDTO cthd = new ChiTietHoaDonDTO();
+    }
+}
