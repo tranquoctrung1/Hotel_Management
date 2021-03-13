8babc92dd18b68835979d7329f31b0245b565ede
tranquoctrung1 <tranquoctrung.12k9@gmail.com>
2021-03-13 12:28:15 +0700

    add PhieuBaoCaoDoanhThu DTO

---
+++ DTO/PhieuCaoBaoDanhThu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Management.DTO
+{
+   public  class PhieuCaoBaoDanhThu
+    {
+        string _maPhieuBaoCao;
+        string _thang;
+        float _doanhThu;
+        string _tiLe;
+        string _maLoaiPhong;
+
+        public string MaPhieuBaoCao { get => _maPhieuBaoCao; set => _maPhieuBaoCao = value; }
+        public string Thang { get => _thang; set => _thang = value; }
+        public float DoanhThu { get => _doanhThu; set => _doanhThu = value; }
+        public string TiLe { get => _tiLe; set => _tiLe = value; }
+        public string MaLoaiPhong { get => _maLoaiPhong; set => _maLoaiPhong = value; }
+    }
+}
